1a0fb86d9ee81076e22216af326c005a92212c18
Derek Bruinooge <derek.bruinooge@gmail.com>
2021-12-18 12:28:45 +0000

    Hashes exercises

---
+++ hashes/hashes_6.rb
@@ -0,0 +1,14 @@
+# Use Enumerable#map to iterate over numbers and return an array containing each number divided by 2. Assign the returned array to a variable named half_numbers and print its value using #p.
+
+# Expected output:
+# [50, 25, 5]
+
+numbers = {
+  high:   100,
+  medium: 50,
+  low:    10
+}
+
+array = numbers.map { |k, v| v / 2 }
+
+p array
