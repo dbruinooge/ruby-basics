dfc0222e781567d4186556ac53530a3cd43087f2
Derek Bruinooge <derek.bruinooge@gmail.com>
2021-12-18 05:21:21 +0000

    Methods exercises

---
+++ methods/methods_7.rb
@@ -0,0 +1,15 @@
+# Based on what the error messages are telling you, update the relevant method definitions and method invocations as necessary so that the names are printed as shown below.
+
+# The dog's name is Spot.
+# The cat's name is Ginger.
+
+def dog(name)
+  return name
+end
+
+def cat(name)
+  return name
+end
+
+puts "The dog's name is #{dog('Spot')}."
+puts "The cat's name is #{cat('Ginger')}."
