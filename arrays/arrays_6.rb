8fc74d6d1a6c2ae7a69153f02362b4973e288e5d
Derek Bruinooge <derek.bruinooge@gmail.com>
2021-12-18 09:48:26 +0000

    Arrays exercises

---
+++ arrays/arrays_6.rb
@@ -0,0 +1,7 @@
+# Use Array#map to iterate over numbers and return a new array with each number doubled. Assign the returned array to a variable named doubled_numbers and print its value using #p
+
+numbers = [1, 2, 3, 4, 5]
+
+doubled_numbers = numbers.map.each { |n| n * 2 }
+
+p doubled_numbers
